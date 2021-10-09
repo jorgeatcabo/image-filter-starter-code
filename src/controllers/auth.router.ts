7f1f9d241875662920accfb3427f85c902f14b08
El Roble <san_lucas2005@yahoo.com>
2021-10-08 18:45:38 -0600

    Add .env functionlaity

--- src/controllers/auth.router.ts
+++ src/controllers/auth.router.ts
@@ -16,7 +16,6 @@ export function requireAuth(req: Request, res: Response, next: NextFunction) {
     if (!req.headers || !req.headers.authorization){
         return res.status(401).send({ message: 'No authorization headers.' });
     }
-    
 
     const token_bearer = req.headers.authorization.split(' ');
     if(token_bearer.length != 2){
@@ -24,10 +23,9 @@ export function requireAuth(req: Request, res: Response, next: NextFunction) {
     }
     
     const token = token_bearer[1];
-
     return jwt.verify(token, config.jwt.secret, (err, decoded) => {
       if (err) {
-        return res.status(500).send({ auth: false, message: 'Failed to authenticate.' });
+        return res.status(500).send({ err:err, auth: false, message: 'Failed to authenticate.' });
       }
       return next();
     });
--- src/server.ts
+++ src/server.ts
@@ -1,3 +1,4 @@
+require('dotenv').config();
 import express from 'express';
 import bodyParser from 'body-parser';
 import {filterImageFromURL, deleteLocalFiles} from './util/util';
